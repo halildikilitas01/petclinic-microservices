e27e8978432bbb56b3096999a96e3978a1b8762b
halildikilitas01 <halildikilitas93@gmail.com>
2023-03-09 13:03:50 +0000

    added jenkinsfile petclinic-weekly-qa for release branch

--- jenkins/deploy_app_on_qa_environment.sh
+++ jenkins/deploy_app_on_qa_environment.sh
@@ -14,4 +14,3 @@ AWS_REGION=$AWS_REGION helm repo update
 AWS_REGION=$AWS_REGION helm upgrade --install \
     petclinic-app-release stable-petclinic/petclinic_chart --version ${BUILD_NUMBER} \
     --namespace petclinic-qa
-```
